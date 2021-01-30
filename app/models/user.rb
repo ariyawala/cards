68d42e7e8635745061b78dd54f0ad43982b46893
ariyawala <ojima574@gmail.com>
2021-01-30 22:23:14 +0900

    モデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mas_many :lists, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
